269800516dbb8a281d0f81bc9d4ca7c6f8fd8630
Mentally Distressed Programmer <126825095+FizzWizZleDazzle@users.noreply.github.com>
2023-12-21 00:57:34 +0000

    more stuff

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ document.getElementById("dropdownButton").addEventListener("mouseenter", functio
 document.getElementById("dropdownButton").addEventListener("mouseleave", function() {
     document.querySelector(".dropdown-content").style.display = "none";
 });
+
+document.getElementById("dropdownButton1").addEventListener("mouseenter", function() {
+    document.querySelector(".dropdown-content").style.display = "block";
+});
+
+document.getElementById("dropdownButton1").addEventListener("mouseleave", function() {
+    document.querySelector(".dropdown-content").style.display = "none";
+});
+
